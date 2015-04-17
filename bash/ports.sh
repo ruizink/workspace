b17a700bbb41faf139af8df28c1d20a1eb65fbf4
Mário Santos <mario.rf.santos@gmail.com>
2015-04-17 22:45:33 +0100

    Removed useless LWM. Stop if lsof isn't available.

--- bash/ports.sh
+++ bash/ports.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 PORT_RANGE_MIN=9000
-PORT_RANGE_MAX=9049
-PORT_RANGE_LWM=0      # port range low water mark
+PORT_RANGE_MAX=9005
 BIND_PORT_TIMEOUT=10
 DEBUG=false
 
 function _isPortAvailable () {
   local port=$1
+  local lsof=$(which lsof 2>&1)
+  [ ! -x "$lsof" ] && echo "Error: Missing lsof utility: Couldn't check for port availability." && exit 127
   $DEBUG && echo "Checking for port ${port} availability..."
-  /usr/sbin/lsof -i -n -P | grep LISTEN | grep -q ":${port}"
+  $lsof -i -n -P | grep LISTEN | grep -q ":${port}"
   if [ $? -ne 0 ]; then
     $DEBUG && echo "Port ${port} is available."
     return 0
@@ -30,23 +31,22 @@ function _createWebServer () {
       $DEBUG && echo "${elapsed_time}s: Port not bound yet..."
       sleep 1 && ((elapsed_time++))
     done
-    [ "$BIND_PORT_TIMEOUT" -le "$elapsed_time" ] && echo "Timeout: Couldn't bind port ${port} after ${BIND_PORT_TIMEOUT} seconds!" && return 1
+    [ "$BIND_PORT_TIMEOUT" -le "$elapsed_time" ] && echo "Timeout: Couldn't bind port ${port} after ${BIND_PORT_TIMEOUT} seconds." && return 1
     echo "Done." && return 0
   fi
   return 1
 }
 
 function _launchWebServer () {
-  [ $PORT_RANGE_LWM -lt $PORT_RANGE_MIN ] && PORT_RANGE_LWM=$PORT_RANGE_MIN
-  for port in $(seq "$PORT_RANGE_LWM" "$PORT_RANGE_MAX"); do
-    _isPortAvailable "$port" && echo "Found available port: ${port}." && _createWebServer "$1" "$port" && return 0
-    ((PORT_RANGE_LWM++))
+  local name=$1
+  for port in $(seq "$PORT_RANGE_MIN" "$PORT_RANGE_MAX"); do
+    _isPortAvailable "$port" && echo "Found available port: ${port}." && _createWebServer "$name" "$port" && return 0
   done
-  echo "Error: Couldn't find available ports within the specified range [${PORT_RANGE_MIN}..${PORT_RANGE_MAX}] !" && return 1
+  echo "Error: Couldn't find available ports within the specified range [${PORT_RANGE_MIN}..${PORT_RANGE_MAX}]" && return 1
 }
 
 #######################################################################################################################################################
 
-for ws in {1..100}; do
+for ws in {1..10} ; do
   _launchWebServer "$ws"
 done
